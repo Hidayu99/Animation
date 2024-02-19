4cdcc0c7c880cdb86bad9869115f0c557a369832
Hidayu Mohamed <105192886+Hidayu99@users.noreply.github.com>
2024-02-19 15:56:53 +0800

    Update script.js

--- script.js
+++ script.js
@@ -1,16 +1,33 @@
 document.addEventListener("DOMContentLoaded", function() {
-    var shopsCarousel = document.querySelector('.shops-carousel');
-    var shops = document.querySelectorAll('.shop');
-    var rotation = 0;
+    var popup = document.getElementById("popup");
+    var popupText = document.getElementById("popup-text");
 
-    setInterval(function() {
-        rotation += 45;
-        shopsCarousel.style.transform = 'rotate(' + rotation + 'deg)';
-    }, 3000);
+    var shops = document.querySelectorAll(".shop");
 
     shops.forEach(function(shop) {
-        shop.addEventListener('click', function() {
-            alert('You clicked on ' + shop.textContent);
+        shop.addEventListener("click", function() {
+            var shopName = shop.textContent;
+            openPopup(shopName);
+            focusOnShop(shop);
         });
     });
+
+    function openPopup(shopName) {
+        popupText.textContent = "You clicked on the " + shopName + " shop!";
+        popup.style.display = "flex";
+    }
+
+    function closePopup() {
+        popup.style.display = "none";
+    }
+
+    function focusOnShop(shop) {
+        var offsetTop = shop.offsetTop;
+        window.scrollTo({
+            top: offsetTop,
+            behavior: "smooth"
+        });
+    }
+
+    window.closePopup = closePopup;
 });
